e9b0604617dec007093fb9e77a1268a1b4bd1b5c
Eduardo Enriquez <eduardo@guruhotel.com>
2024-01-19 21:03:24 -0600

    chore(env): adding export script for env vars

---
+++ function/export-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DEFAULT_AWS_REGION="us-east-1"
+export S3_BUCKET="stori-storage-transaction-file"
+export ENV="development"
+export TABLE_NAME="<REPLACE WITH YOURS>"
+export RESEND_API_KEY="<REPLACE WITH YOURS>"
+export _LAMBDA_SERVER_PORT=8080
+export AWS_LAMBDA_RUNTIME_API=3.9
